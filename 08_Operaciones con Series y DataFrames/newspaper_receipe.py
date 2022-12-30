0c092a9b2fc538a3a7a43ffe015df8f4c9449668
m4r11cruz <31555669+m4r11cruz@users.noreply.github.com>
2022-12-30 13:15:49 -0500

    Add files via upload

---
+++ 08_Operaciones con Series y DataFrames/newspaper_receipe.py
@@ -0,0 +1,75 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+from urllib.parse import urlparse
+
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def main(filename):
+    logger.info('Starting cleaning process')
+
+    df = _read_data(filename)
+    newspaper_uid = _extract_newspaper_uid(filename)
+    df = _add_newspaper_uid_column(df, newspaper_uid)
+    df = _extract_host(df)
+    df = _fill_missing_titles(df)
+
+    return df
+
+
+def _read_data(filename):
+    logger.info('Reading file {}'.format(filename))
+
+    return pd.read_csv(filename)
+
+
+def _extract_newspaper_uid(filename):
+    logger.info('Extracting newspaper uid')
+    newspaper_uid = filename.split('_')[0]
+
+    logger.info('Newspaper uid detected: {}'.format(newspaper_uid))
+    return newspaper_uid
+
+
+def _add_newspaper_uid_column(df, newspaper_uid):
+    logger.info('Filling newspaper_uid column with {}'.format(newspaper_uid))
+    df['newspaper_uid'] = newspaper_uid
+
+    return df
+
+
+def _extract_host(df):
+    logger.info('Extracting host from urls')
+    df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+
+    return df
+
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles')
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ''.join(title_word_list))
+                     )
+
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+
+    return df
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',
+                        help='The path to the dirty data',
+                        type=str)
+
+    args = parser.parse_args()
+
+    df = main(args.filename)
+    print(df)
